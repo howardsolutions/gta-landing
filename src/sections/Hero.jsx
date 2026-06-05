c9ed54fbd05b84b7f08837bacafa863a5fad61f6
howardsolutions <phungthinh0946519356@gmail.com>
2026-06-05 15:22:22 +0700

    feat: coming soon section & constants for media query

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,1 +1,36 @@
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
 
+function Hero() {
+  return (
+    <section className='hero-section'>
+      <div className='size-full mask-wrapper'>
+        <img
+          src='/images/hero-bg.webp'
+          alt='background of the hero section'
+          className='scale-out'
+        />
+        <img
+          src='/images/hero-text.webp'
+          alt='hero-logo'
+          className='title-logo fade-out'
+        />
+        <img
+          src='/images/watch-trailer.png'
+          alt='Trailer'
+          className='trailer-logo fade-out'
+        />
+        {/* Play btn */}
+        <div className='play-img fade-out'>
+          <img src='/images/play.png' alt='play' className='w-7 ml-1' />
+        </div>
+      </div>
+
+      <div>
+        <img src='' alt='' />
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
